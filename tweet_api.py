4341d462824585b9efead84de7ad68d43d795052
Colm1711 <cosullivan1711@gmail.com>
2022-05-16 23:46:09 +0000

    Add class name, add inheritance to polarity func

--- tweet_api.py
+++ tweet_api.py
@@ -26,7 +26,7 @@ auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 # pass in authentication to API
 api = tweepy.API(auth)
 
-search_term = "googl" 
+ 
 
 # emoji regex search - this was sourced from
 pattern = re.compile(
@@ -45,96 +45,99 @@ pattern = re.compile(
     "])"
   )
 
-# # testing with public tweets from profile
-def get_tweets_from_timeline():
-    """
-    Description:
+class Tweet_Sentiment():
 
-    This function fetched the tweets from the homepage of twitter account.
+    # testing with public tweets from profile
+    def get_tweets_from_timeline(search_term):
+        """
+        Description:
 
-    Params:
-            none
+        This function fetched the tweets from the homepage of twitter account.
 
-    Returns:
-            str
+        Params:
+                str - search_term
 
-    """
-    # have set language to english and limited the search to first 200
-    tweets_timeline = api.search_tweets(q=search_term, lang='en', count=2000, tweet_mode='extended')
-    return tweets_timeline
+        Returns:
+                str
+
+        """
+        # have set language to english and limited the search to first 200
+        tweets_timeline = api.search_tweets(q=search_term, lang='en', count=2000, tweet_mode='extended')
+        return tweets_timeline
 
 
-# Clean data of retweets, hastags, username handles and hyperlinks
-def clean_tweets(tweets_to_clean):
-    """
-    Description:
+    # Clean data of retweets, hastags, username handles and hyperlinks
+    def clean_tweets(tweets_to_clean):
+        """
+        Description:
 
-    This function cleans the tweets fetched from the homepage of twitter account.
+        This function cleans the tweets fetched from the homepage of twitter account.
 
-    Params:
-            str
+        Params:
+                str
 
-    Returns:
-            list(str's)
+        Returns:
+                list(str's)
 
-    """
-    # list to populate cleaned tweets
-    tweet_list = []
+        """
+        # list to populate cleaned tweets
+        tweet_list = []
 
-    tweets_dict = {tweet.full_text for tweet in tweets_to_clean}
-    for tweet in tweets_dict:
-        # removing retweets from tweets
-        clean_text = tweet.replace('RT', '')
-        # removing new line chars from tweets
-        clean_text = clean_text.replace('\n', '')
-        # removing hashtags from tweets
-        clean_text = clean_text.replace('#', '')
-        # removing twitter handles from tweets
-        if clean_text.startswith('@'):
-            remove_at = clean_text.index(' ')
-            clean_text = clean_text[remove_at+2:]
-        # removing twitter hanldes with leading spaces
-        if clean_text.startswith(' @'):
-            remove_space_at = clean_text.index(':')
-            clean_text = clean_text[remove_space_at+2:]
-        # removing any links
-        clean_text = re.sub('http://\S+|https://\S+', '', clean_text)
-        # removing any emojis, the list was sourced externally. Ref in README file.
-        clean_text = re.sub(pattern, ' ', clean_text)
-        tweet_list.append(clean_text)
-    return tweet_list
+        tweets_dict = {tweet.full_text for tweet in tweets_to_clean}
+        for tweet in tweets_dict:
+            # removing retweets from tweets
+            clean_text = tweet.replace('RT', '')
+            # removing new line chars from tweets
+            clean_text = clean_text.replace('\n', '')
+            # removing hashtags from tweets
+            clean_text = clean_text.replace('#', '')
+            # removing twitter handles from tweets
+            if clean_text.startswith('@'):
+                remove_at = clean_text.index(' ')
+                clean_text = clean_text[remove_at+2:]
+            # removing twitter hanldes with leading spaces
+            if clean_text.startswith(' @'):
+                remove_space_at = clean_text.index(':')
+                clean_text = clean_text[remove_space_at+2:]
+            # removing any links
+            clean_text = re.sub('http://\S+|https://\S+', '', clean_text)
+            # removing any emojis, the list was sourced externally. Ref in README file.
+            clean_text = re.sub(pattern, ' ', clean_text)
+            tweet_list.append(clean_text)
+        return tweet_list
 
-def polarity_analysis(data):
-    """
-    Description:
+    def polarity_analysis(data):
+        """
+        Description:
 
-    This function scans the cleaned tweets and returns either positive, negative or neutral polarity status.
+        This function scans the cleaned tweets and returns either positive, negative or neutral polarity status.
 
-    Params:
-        list(str)
+        Returns:
+            f string of string data
 
-    Returns:
-        f string of string data
+        """   
+    
+        polarity = 0
+        positive = 0
+        negative = 0
+        neutral = 0
 
-    """   
- 
-    polarity = 0
-    positive = 0
-    negative = 0
-    neutral = 0
-    # unpacks list for individual tweets
-    for tweet in data:
-        # creating textblob object for analysis
-        analysis = TextBlob(tweet)
-        # using in built func to analyze polarity of each tweet
-        tweet_data_polarity = analysis.polarity
-        # assigning positive, negative or neutral based on polarity score
-        if tweet_data_polarity > 0:
-            positive += 1
-        elif tweet_data_polarity < 0:
-            negative += 1
-        else:
-            neutral += 1
-        polarity += analysis.polarity
-    return f'Polarity for {search_term} is: {polarity}\nAmount of positive: {positive}\nAmount of negative: {negative}\nAmount of neutral: {neutral}'
+        t_data = Tweet_Sentiment.get_tweets_from_timeline(data)
+        data_t = Tweet_Sentiment.clean_tweets(t_data)
+
+        # unpacks list for individual tweets
+        for tweet in data_t:
+            # creating textblob object for analysis
+            analysis = TextBlob(tweet)
+            # using in built func to analyze polarity of each tweet
+            tweet_data_polarity = analysis.polarity
+            # assigning positive, negative or neutral based on polarity score
+            if tweet_data_polarity > 0:
+                positive += 1
+            elif tweet_data_polarity < 0:
+                negative += 1
+            else:
+                neutral += 1
+            polarity += analysis.polarity
+        return f'Polarity for {data} is: {polarity}\nAmount of positive: {positive}\nAmount of negative: {negative}\nAmount of neutral: {neutral}'
 
