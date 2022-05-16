3f21e78021de2aeb0c6ee78dd158dbdc8a1da7cc
Colm1711 <cosullivan1711@gmail.com>
2022-05-16 13:23:11 +0000

    add comments to code

--- tweet_api.py
+++ tweet_api.py
@@ -78,20 +78,28 @@ def clean_tweets(tweets_to_clean):
             list(str's)
 
     """
+    # list to populate cleaned tweets
     tweet_list = []
 
     tweets_dict = {tweet.full_text for tweet in tweets_to_clean}
     for tweet in tweets_dict:
+        # removing retweets from tweets
         clean_text = tweet.replace('RT', '')
+        # removing new line chars from tweets
         clean_text = clean_text.replace('\n', '')
+        # removing hashtags from tweets
         clean_text = clean_text.replace('#', '')
+        # removing twitter handles from tweets
         if clean_text.startswith('@'):
-            remove = clean_text.index(' ')
-            clean_text = clean_text[remove+2:]
+            remove_at = clean_text.index(' ')
+            clean_text = clean_text[remove_at+2:]
+        # removing twitter hanldes with leading spaces
         if clean_text.startswith(' @'):
-            remove = clean_text.index(':')
-            clean_text = clean_text[remove+2:]
+            remove_space_at = clean_text.index(':')
+            clean_text = clean_text[remove_space_at+2:]
+        # removing any links
         clean_text = re.sub('http://\S+|https://\S+', '', clean_text)
+        # removing any emojis, the list was sourced externally. Ref in README file.
         clean_text = re.sub(pattern, ' ', clean_text)
         tweet_list.append(clean_text)
     return tweet_list
@@ -106,7 +114,7 @@ def polarity_analysis(data):
         list(str)
 
     Returns:
-        tuple(float + str)
+        f string of string data
 
     """   
  
@@ -114,10 +122,13 @@ def polarity_analysis(data):
     positive = 0
     negative = 0
     neutral = 0
-
+    # unpacks list for individual tweets
     for tweet in data:
+        # creating textblob object for analysis
         analysis = TextBlob(tweet)
+        # using in built func to analyze polarity of each tweet
         tweet_data_polarity = analysis.polarity
+        # assigning positive, negative or neutral based on polarity score
         if tweet_data_polarity > 0:
             positive += 1
         elif tweet_data_polarity < 0:
